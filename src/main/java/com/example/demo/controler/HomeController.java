22382bc6a4de2f94413d89f66881590023be3729
Sneider Jurgensen <132455333+Basilik06@users.noreply.github.com>
2025-05-13 10:02:08 -0400

    modificaciones
    se agregaron los mapeados

--- src/main/java/com/example/demo/controler/HomeController.java
+++ src/main/java/com/example/demo/controler/HomeController.java
@@ -7,10 +7,23 @@ public class HomeController {
 
     @GetMapping("/")
     public String redirectToInicio() {
-        return "admin/index";
+        return "blog/index";
+    }
+     @GetMapping("/login")
+    public String redirectlogin() {
+        return "blog/login"; // sin ".html"
+    }
+
+    @GetMapping("/index_crud")
+    public String redirectadmin() {
+        return "admin/index"; // sin ".html"
     }
     @GetMapping("/lista-monitores")
     public String mostrarListaMonitores() {
         return "admin/lista-monitores"; // sin ".html"
+    }
+    @GetMapping("/usuarios-monitores")
+    public String redirectusuarios() {
+        return "admin/usuarios"; // sin ".html"
     }
 }
